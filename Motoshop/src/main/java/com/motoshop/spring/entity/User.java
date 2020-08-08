3cca51f6bf070726d8d66f7762b3b5a18129414f
hoan2403 <quochoan2403@gmail.com>
2020-08-08 14:43:34 +0700

    - added user repository

--- Motoshop/src/main/java/com/motoshop/spring/entity/User.java
+++ Motoshop/src/main/java/com/motoshop/spring/entity/User.java
@@ -1,10 +1,7 @@
 package com.motoshop.spring.entity;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity(name = "User")
